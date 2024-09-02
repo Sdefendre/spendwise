244949fd13eaa5dd09b0a34a0d2795af9d95a1ad
joe Defendre <jchacker5@gmail.com>
2024-09-02 19:35:46 -0400

    added hover to card ui elements

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm transition-all duration-200 ease-in-out hover:shadow-md hover:scale-105",
       className
     )}
     {...props}
